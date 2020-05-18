d3952f0b15c6b25a85102ed354bd07ae015df8d7
james00718 <yaok0710@163.com>
2020-05-18 10:47:31 +0800

    fixed chinese app bugs

--- Release.sh
+++ Release.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 BUILDPATH=`pwd`
-ANDROIDDM_VERSION=1.1
+ANDROIDDM_VERSION=$1
 NOVNC_VERSION=1.0
 MQTT_VERSION=1.0
 
+if [ $# -ne 1 ]; then
+	echo "Usage: ./Release.sh [version]"
+	echo "Example: ./Release.sh 1.2"
+	exit 1
+fi
 
 docker tag edgesolution/androiddm-mosquitto-dev:v$MQTT_VERSION edgesolution/androiddm-mosquitto:v$MQTT_VERSION
 
@@ -13,7 +18,7 @@ docker tag edgesolution/androiddm-javaenv-slim-dev:v$ANDROIDDM_VERSION edgesolut
 
 docker tag edgesolution/androiddm-astore-slim-dev:v$ANDROIDDM_VERSION edgesolution/androiddm-astore-slim:v$ANDROIDDM_VERSION
 
-
+# push image to dockerhub
 docker login && \
 docker push edgesolution/androiddm-mosquitto:v$MQTT_VERSION && \
 docker push edgesolution/androiddm-novnc:v$NOVNC_VERSION && \
