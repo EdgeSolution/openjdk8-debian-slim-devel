e68565bf33b9fc0d2cae0e8346a3c911790caa21
gangqiang.sun(孙刚强) <gangqiang.sun@advantech.com.cn>
2020-05-20 11:40:28 +0800

    modify ready.sh

--- astore/build-docker-image/ready.sh
+++ astore/build-docker-image/ready.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-rm astore.jar 
-cp /home/gangqiangsun/MYPROJECT/BACKEND/m2m-management/target/management-0.0.1-SNAPSHOT.jar astore.jar
-ls -al
+ajarPath=/home/gangqiangsun/MYPROJECT/LOCAL/openjdk8-debian-slim-devel/astore/build-docker-image
+rm $ajarPath/astore.jar 
+cp /home/gangqiangsun/MYPROJECT/BACKEND/m2m-management/target/management-0.0.1-SNAPSHOT.jar $ajarPath/astore.jar
+ls -al $ajarPath
--- ready.sh
+++ ready.sh
@@ -7,3 +7,10 @@ ls -al $jarPath
 rm -r $jarPath/androidDM/*
 cp -r /home/gangqiangsun/MYPROJECT/FRONTEND/m2mlink/dist/* $jarPath/androidDM/
 ls -al $jarPath/androidDM/
+
+
+ajarPath=/home/gangqiangsun/MYPROJECT/LOCAL/openjdk8-debian-slim-devel/astore/build-docker-image
+rm $ajarPath/astore.jar 
+cp /home/gangqiangsun/MYPROJECT/BACKEND/m2m-management/target/management-0.0.1-SNAPSHOT.jar $ajarPath/astore.jar
+ls -al $ajarPath
+
