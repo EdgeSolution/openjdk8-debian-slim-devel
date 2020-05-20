e57c66f3f45a494a04a9b3d169259d9d43bb0320
gangqiang.sun(孙刚强) <gangqiang.sun@advantech.com.cn>
2020-05-20 11:44:06 +0800

    add release version and local version

--- Release.sh
+++ Release.sh
@@ -1,27 +1,29 @@
 #!/bin/sh
 BUILDPATH=`pwd`
 ANDROIDDM_VERSION=$1
+ANDROIDDM_RELEASE_VERSION=$2
 NOVNC_VERSION=1.0
 MQTT_VERSION=1.0
 
-if [ $# -ne 1 ]; then
-	echo "Usage: ./Release.sh [version]"
-	echo "Example: ./Release.sh 1.2"
+if [ $# -ne 2 ]; then
+	echo "Usage: ./Release.sh [version] [releaseversion]"
+	echo "Example: ./Release.sh 1.2 1.1"
 	exit 1
 fi
-
+echo "version=${ANDROIDDM_VERSION}"
+echo "release version=${ANDROIDDM_RELEASE_VERSION}"
 docker tag edgesolution/androiddm-mosquitto-dev:v$MQTT_VERSION edgesolution/androiddm-mosquitto:v$MQTT_VERSION
 
 docker tag edgesolution/androiddm-novnc-dev:v$NOVNC_VERSION edgesolution/androiddm-novnc:v$NOVNC_VERSION
 
-docker tag edgesolution/androiddm-javaenv-slim-dev:v$ANDROIDDM_VERSION edgesolution/androiddm-javaenv-slim:v$ANDROIDDM_VERSION
+docker tag edgesolution/androiddm-javaenv-slim-dev:v$ANDROIDDM_VERSION edgesolution/androiddm-javaenv-slim:v$ANDROIDDM__RELEASE_VERSION
 
-docker tag edgesolution/androiddm-astore-slim-dev:v$ANDROIDDM_VERSION edgesolution/androiddm-astore-slim:v$ANDROIDDM_VERSION
+docker tag edgesolution/androiddm-astore-slim-dev:v$ANDROIDDM_VERSION edgesolution/androiddm-astore-slim:v$ANDROIDDM_RELEASE_VERSION
 
 # push image to dockerhub
 docker login && \
 docker push edgesolution/androiddm-mosquitto:v$MQTT_VERSION && \
 docker push edgesolution/androiddm-novnc:v$NOVNC_VERSION && \
-docker push edgesolution/androiddm-javaenv-slim:v$ANDROIDDM_VERSION && \
-docker push edgesolution/androiddm-astore-slim:v$ANDROIDDM_VERSION
+docker push edgesolution/androiddm-javaenv-slim:v$ANDROIDDM_RELEASE_VERSION && \
+docker push edgesolution/androiddm-astore-slim:v$ANDROIDDM_RELEASE_VERSION
 
