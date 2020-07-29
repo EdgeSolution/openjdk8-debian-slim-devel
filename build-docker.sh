67fdfc70f2a8640a78d3f030b69ecc1f02ee1f85
gangqiang.sun(孙刚强) <gangqiang.sun@advantech.com.cn>
2020-07-29 14:01:18 +0800

    create branch dev

--- build-docker.sh
+++ build-docker.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
 BUILDPATH=`pwd`
-ANDROIDDM_VERSION=1.6.5
+ANDROIDDM_VERSION=1.6.0
 NOVNC_VERSION=1.0
 MQTT_VERSION=1.0
+HARBORURL=172.21.73.113
 
 cd $BUILDPATH/mosquitto/
-docker build -t 00718/androiddm-mosquitto-dev:v$MQTT_VERSION .
+docker build -t $HARBORURL/androiddm-dev/androiddm-mosquitto-dev:v$MQTT_VERSION .
 
 cd $BUILDPATH/noVnc
-docker build -t 00718/androiddm-novnc-dev:v$NOVNC_VERSION .
+docker build -t $HARBORURL/androiddm-dev/androiddm-novnc-dev:v$NOVNC_VERSION .
 
 cd $BUILDPATH/java-env/build-docker-image/
-docker build -t 00718/androiddm-javaenv-slim-dev:v$ANDROIDDM_VERSION .
+docker build -t $HARBORURL/androiddm-dev/androiddm-javaenv-slim-dev:v$ANDROIDDM_VERSION .
 
 cd $BUILDPATH/astore/build-docker-image/
-docker build -t 00718/androiddm-astore-slim-dev:v$ANDROIDDM_VERSION .
-
-docker-compose up -d
+docker build -t $HARBORURL/androiddm-dev/androiddm-astore-slim-dev:v$ANDROIDDM_VERSION .
--- ready.sh
+++ ready.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-jarPath=/home/gangqiangsun/MYPROJECT/LOCAL/openjdk8-debian-slim-devel/java-env/build-docker-image
+jarPath=/home/gangqiangsun/MYPROJECT/LOCAL/openjdk8-debian-slim-test/java-env/build-docker-image
 rm $jarPath/m2mserver.jar 
-cp /home/gangqiangsun/MYPROJECT/BACKEND/m2m-core/leshan-mqtt-server-demo/target/leshan-mqtt-server-demo-1.0.0-ADV-jar-with-dependencies.jar $jarPath/m2mserver.jar
+cp /home/gangqiangsun/MYPROJECT/BACKEND/m2m-core-linux/leshan-mqtt-server-demo/target/leshan-mqtt-server-demo-1.0.0-ADV-jar-with-dependencies.jar $jarPath/m2mserver.jar
 ls -al $jarPath
 rm -r $jarPath/androidDM/*
-cp -r /home/gangqiangsun/MYPROJECT/FRONTEND/m2mlink/dist/* $jarPath/androidDM/
+cp -r /home/gangqiangsun/MYPROJECT/FRONTEND/m2mlink-linux/dist/* $jarPath/androidDM/
 ls -al $jarPath/androidDM/
 
 
-ajarPath=/home/gangqiangsun/MYPROJECT/LOCAL/openjdk8-debian-slim-devel/astore/build-docker-image
+ajarPath=/home/gangqiangsun/MYPROJECT/LOCAL/openjdk8-debian-slim-test/astore/build-docker-image
 rm $ajarPath/astore.jar 
-cp /home/gangqiangsun/MYPROJECT/BACKEND/m2m-management/target/management-0.0.1-SNAPSHOT.jar $ajarPath/astore.jar
+cp /home/gangqiangsun/MYPROJECT/BACKEND/m2m-management-linux/target/management-0.0.1-SNAPSHOT.jar $ajarPath/astore.jar
 ls -al $ajarPath
 
