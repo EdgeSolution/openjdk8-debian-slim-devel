162eeddc05d1f966349da08176cfad4a34d3ddc2
gangqiang.sun(孙刚强) <gangqiang.sun@advantech.com.cn>
2020-07-31 11:25:29 +0800

    add aliyun repo and local repo

---
+++ build-docker-aliyun.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+BUILDPATH=`pwd`
+ANDROIDDM_VERSION=1.0.0
+NOVNC_VERSION=1.0
+MQTT_VERSION=1.0
+HARBORURL=172.21.73.113
+REPO=androiddm-aliyun
+
+cd $BUILDPATH/mosquitto/
+docker build -t $HARBORURL/$REPO/androiddm-mosquitto-aliyun:v$MQTT_VERSION .
+
+cd $BUILDPATH/noVnc
+docker build -t $HARBORURL/$REPO/androiddm-novnc-aliyun:v$NOVNC_VERSION .
+
+cd $BUILDPATH/java-env/build-docker-image/
+docker build -t $HARBORURL/$REPO/androiddm-javaenv-slim-aliyun:v$ANDROIDDM_VERSION .
+
+cd $BUILDPATH/astore/build-docker-image/
+docker build -t $HARBORURL/$REPO/androiddm-astore-slim-aliyun:v$ANDROIDDM_VERSION .
--- build-docker.sh
+++ build-docker.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 BUILDPATH=`pwd`
-ANDROIDDM_VERSION=1.6.5
+ANDROIDDM_VERSION=1.6.7
 NOVNC_VERSION=1.0
 MQTT_VERSION=1.0
+HARBORURL=172.21.73.113
+REPO=androiddm
 
 cd $BUILDPATH/mosquitto/
-docker build -t 00718/androiddm-mosquitto-dev:v$MQTT_VERSION .
+docker build -t $HARBORURL/$REPO/androiddm-mosquitto-dev:v$MQTT_VERSION .
 
 cd $BUILDPATH/noVnc
-docker build -t 00718/androiddm-novnc-dev:v$NOVNC_VERSION .
+docker build -t $HARBORURL/$REPO/androiddm-novnc-dev:v$NOVNC_VERSION .
 
 cd $BUILDPATH/java-env/build-docker-image/
-docker build -t 00718/androiddm-javaenv-slim-dev:v$ANDROIDDM_VERSION .
+docker build -t $HARBORURL/$REPO/androiddm-javaenv-slim-dev:v$ANDROIDDM_VERSION .
 
 cd $BUILDPATH/astore/build-docker-image/
-docker build -t 00718/androiddm-astore-slim-dev:v$ANDROIDDM_VERSION .
+docker build -t $HARBORURL/$REPO/androiddm-astore-slim-dev:v$ANDROIDDM_VERSION .
 
-docker-compose up -d
